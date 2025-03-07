5a524edfb55223aa4a5cf245848791ce15844bcc
mhsong <mhsong@kookmin.ac.kr>
2025-03-07 09:33:27 +0900

    Fix ami data source

--- basic_web_service/data.tf
+++ basic_web_service/data.tf
@@ -1,6 +1,16 @@
 # ubuntu 24.04 ami data source
 data "aws_ami" "ubuntu_24_04" {
   most_recent = true
-  owners      = ["099720109487"]
-  name_regex  = "^ubuntu/images/hvm-ssd-gp3/ubuntu-jammy-24.04-amd64-server-*"
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
